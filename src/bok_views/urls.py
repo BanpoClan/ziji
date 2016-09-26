fd2f3dea0a9cecd1796d03ddc044f38c3feef778
zcx <zcx@qq.com>
2016-09-26 08:22:03 +0800

    添加登录页面

--- src/bok_views/urls.py
+++ src/bok_views/urls.py
@@ -1,8 +1,11 @@
 from django.conf.urls import url, include
 
 from bok_views import index_views
+from bok_views import user_views
 
 urlpatterns = [
     url(r'^index/', index_views.index),
     url(r'^main_page/', index_views.main_page),
+    url(r'^login/', user_views.login),
+    url(r'^login_action/', user_views.login_action),
 ]
